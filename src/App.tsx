183664b8f7e66177c5ffb105a115266bd66c06d0
zoltan <zoltan.lukacs@bestit.de>
2022-07-08 13:57:24 +0200

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,21 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { Matches } from './components'
 
-import { Routes, Route } from "react-router-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
+import '../node_modules/bootstrap/dist/js/bootstrap'
 
 import './App.css'
 
-import Index from "./pages/index/index.page";
-import Pricing from "./pages/pricing/pricing.page";
-import Company from "./pages/company/company.page";
-import Contact from "./pages/contact/contact.page";
-
-import Header from "./components/header/header.component";
-
 const App = () => {
+
   return (
     <div className='app'>
-        <Header/>
-
         <div className='main' role='main'>
-            <Routes>
-                <Route path="/" element={ <Index/> }/>
-                <Route path="/pricing" element={ <Pricing/> }/>
-                <Route path="/company" element={ <Company/> }/>
-                <Route path="/contact" element={ <Contact/> }/>
-            </Routes>
+            <Matches/>
         </div>
     </div>
-  );
+  )
 }
 
 export default App
